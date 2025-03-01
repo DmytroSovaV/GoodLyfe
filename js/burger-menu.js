8dce479aa0931afce25743f59adfd3e9ff1a103a
DmytroSovaV <ikrosua@gmail.com>
2025-03-01 18:27:05 +0200

    first commit

---
+++ js/burger-menu.js
@@ -0,0 +1,9 @@
+const burgerMenu = document.querySelector('.mobile-menu');
+
+const burgerMenuOpen = document.querySelector('.menu-btn-open')
+const burgerMenuClose = document.querySelector('.menu-btn-close')
+
+const toggleBurger = () => burgerMenu.classList.toggle('is-open');
+
+burgerMenuOpen.addEventListener('click', toggleBurger);
+burgerMenuClose.addEventListener('click', toggleBurger);
